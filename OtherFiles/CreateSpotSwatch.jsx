cd79cc3759f3a775e5109974d17ca0a140fce0f7
Joe Bavitz <Joseph.Bavitz@bicworld.com>
2021-06-21 08:50:42 -0400

    new files added.

---
+++ OtherFiles/CreateSpotSwatch.jsx
@@ -0,0 +1,58 @@
+﻿/**********************************************************
+
+ADOBE SYSTEMS INCORPORATED 
+Copyright 2005-2010 Adobe Systems Incorporated 
+All Rights Reserved 
+
+NOTICE:  Adobe permits you to use, modify, and 
+distribute this file in accordance with the terms
+of the Adobe license agreement accompanying it.  
+If you have received this file from a source 
+other than Adobe, then your use, modification,
+or distribution of it requires the prior 
+written permission of Adobe. 
+
+*********************************************************/
+
+/**********************************************************
+ 
+CreateSpotSwatch.jsx
+
+DESCRIPTION
+
+Create a new spot color swatch in the general swatch group
+and apply a tint of the new spot swatch to a new path item.
+ 
+**********************************************************/
+
+var docRef = app.documents.add(DocumentColorSpace.CMYK);
+
+var spotName = "CreateSpotSwatch";
+
+// Create CMYKColor
+var cmykColor = new CMYKColor();
+var c = prompt("Enter the Cyan percentage.");
+var m = prompt("Enter the Magenta percentage.");
+var y = prompt("Enter the Yellow percentage.");
+var k = prompt("Enter the Black percentage.");
+cmykColor.cyan = c;
+cmykColor.magenta = m;
+cmykColor.yellow = y;
+cmykColor.black = k;
+
+// Create Spot
+var spot = docRef.spots.add();
+spot.color = cmykColor;
+spot.colorType = ColorModel.SPOT;
+spot.name = spotName;
+
+// Create new SpotColor using Spot created above and apply a 50% tint
+var spotColor = new SpotColor();
+spotColor.tint = 100; // 50% tint
+spotColor.spot = spot;
+
+// Apply the tinted spot swatch to a new path item
+var pathRef = docRef.pathItems.rectangle (400, 200, 200, 100, false);
+pathRef.filled = true;
+pathRef.fillColor = spotColor;
+pathRef.stroked = true;
---
+++ OtherFiles/Javascript goals.jsx
@@ -0,0 +1,118 @@
+﻿//sets the ruler origin of the open document to its default.
+document.rulerOrigin = [0,0];
+
+
+//creates the initial pop-up menus for the user to enter the order information.
+var proofRecipientName = prompt("Proof Recipient Name.");
+var proofRecipientEmailAddress = prompt("Proof Recipient Email Address.");
+var originalArtFile = prompt("The file name of the original art file.");
+var jde = prompt("The JDE order number.");
+var po = prompt("The customer's purchase order number (if any).");
+if (po= "undefined") {
+    var po = "N/A";
+    }
+var fileNameDescription = prompt("The file name's description.");
+var bodyColor = prompt("What is the body color?");
+var version = prompt("What is this version?");
+var fileName = jde +"_"+fileNameDescription+"_"+bodyColor+"_"+version;
+
+
+
+
+//makes the lighter body color layer visible for the lighter body color specified in the initial pop-up menu.
+#target illustrator;
+function bodyColorLayerVisible (a) {
+    var doc = app.activeDocument;
+    var layers = docShow.layers;
+    var showLayer = a;
+    if (bodyColor === "Orange") {
+        try {
+            showLayer = layers.getByName (Orange);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+        } else if (bodyColor === "White") {
+        try {
+            showLayer = layers.getByName (White);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Green") {
+        try {
+            showLayer = layers.getByName (Green);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "LightBlue") {
+        try {
+            showLayer = layers.getByName (LightBlue);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Yellow") {
+        try {
+            showLayer = layers.getByName (Yellow);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Black") {
+        try {
+            showLayer = layers.getByName (Black);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Blue") {
+        try {
+            showLayer = layers.getByName (Blue);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Pink") {
+        try {
+            showLayer = layers.getByName (Pink);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "Red") {
+        try {
+            showLayer = layers.getByName (Red);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+                } else if (bodyColor === "DarkGreen") {
+        try {
+            showLayer = layers.getByName (DarkGreen);
+            showLayer.visible = true;
+            redraw();
+            }
+        catch (e) {}
+
+//enters the current day and date in the date text box.
+
+
+
+
+//delete all swatches in the swatch window. Create a new, empty swatch group. Inserts the swatches entered in the initial pop-up menu into the new swatch group.
+
+
+
+
+//save the document as a PDF file with the name specified in the initial pop-up menu into the '3 - Art Proof Sent' folder.
+ document.saveAs
+
+
+
+//copies the name of the file to the clipboard.
+
+
+
+
